edf8a57b729d20a28f8ac604f712a84ce682c526
Hugo Lemoine <hlemoine@spypoint.com>
2023-02-06 21:07:24 -0500

    WIP: enities moving to food

--- rlvideogame/entities/entities.py
+++ rlvideogame/entities/entities.py
@@ -1,5 +1,6 @@
 import enum
 import logging
+import math
 
 import pygame
 
@@ -9,42 +10,80 @@ berry_image = pygame.transform.scale(berry_image, (20, 20))
 
 
 class EntityType(enum.Enum):
-    ITEM = 1
-    CREATURE = 2
+    NOTHING = 1
+    ITEM = 2
+    CREATURE = 3
 
 
-class Creature(pygame.sprite.Sprite):
-    def __init__(self, pos: pygame.math.Vector2, dir: pygame.math.Vector2):
+class EntityState(enum.Enum):
+    DEAD = 1
+    ALIVE = 2
+
+
+class BaseEntity(pygame.sprite.Sprite):
+    def __init__(self, pos: pygame.math.Vector2, dir: pygame.math.Vector2) -> None:
         super().__init__()
+
+        self.entity_type = EntityType.NOTHING
+        self.entiy_state = EntityState.DEAD
+        self.direction = dir
+        self.position = pos
+        self.next_position = pos
+        self.max_speed = 2.
+        self.max_angle = 1. # degrees
+    
+    def move(self, mov: pygame.math.Vector2):
+        movement = mov.copy()
+        movement_distance = movement.length()
+        if movement_distance > self.max_speed:
+            movement_distance = self.max_speed
+            movement.scale_to_length(movement_distance)
+        movement_angle = movement.angle_to(self.direction) - 180
+        if abs(movement_angle) > self.max_angle:
+            movement = self.direction.rotate(self.max_angle) if movement_angle > 0 else self.direction.rotate(-self.max_angle)
+            movement.scale_to_length(movement_distance)
+        self.next_position += mov
+    
+    def flush_move(self):
+        self.next_position = self.position
+
+    def update(self, *args, **kwargs) -> None:
+        super().update(*args, **kwargs)
+        self.position = self.next_position
+
+
+class Creature(BaseEntity):
+    def __init__(self, pos: pygame.math.Vector2, dir: pygame.math.Vector2) -> None:
+        super().__init__(pos, dir)
         self.logger = logging.getLogger(__class__.__name__)
 
         self.entity_type = EntityType.CREATURE
-        self.direction = dir
-        self.position = pos
+        self.entiy_state = EntityState.ALIVE
 
         # Set the color of the circle
         self.color = (0, 255, 0)
 
         # Create the image and rect attributes
-        self.image = pygame.Surface((20, 20))
-        self.image.fill((255, 255, 255))
+        # self.image = pygame.Surface((20, 20))
+        # self.image.fill((255, 255, 255))
+        self.image = pygame.Surface((20, 20), pygame.SRCALPHA, 32)
+        self.image = self.image.convert_alpha()
         pygame.draw.circle(self.image, (0, 255, 0), (10, 10), 10)
         self.rect = self.image.get_rect()
         self.rect.center = self.position
-    
+
+
     def update(self) -> None:
-        self.direction.rotate_ip(1)
-        self.position += self.direction
         self.rect.center = self.position
 
 
-class Food(pygame.sprite.Sprite):
-    def __init__(self, pos: pygame.math.Vector2, dir: pygame.math.Vector2):
-        super().__init__()
+class Food(BaseEntity):
+    def __init__(self, pos: pygame.math.Vector2, dir: pygame.math.Vector2) -> None:
+        super().__init__(pos, dir)
 
         self.entity_type = EntityType.ITEM
-        self.direction = dir
+        self.entiy_state = EntityState.ALIVE
 
         self.image = berry_image
         self.rect = self.image.get_rect()
-        self.rect.move_ip(pos)
+        self.rect.move_ip(self.position)
--- rlvideogame/game.py
+++ rlvideogame/game.py
@@ -18,32 +18,33 @@ class Wolrd():
 
         self.logger = logging.getLogger(__class__.__name__)
 
+        # Create the window
+        self.screen = pygame.display.set_mode(screen_size)
+
         # Create a clock to control the frame rate
         self.clock = pygame.time.Clock()
         self.fps = fps
 
         self.screen_size = screen_size
         self.dot_speed = 1
-        self.sprites_group = pygame.sprite.Group()
-        self.sprites_group.add(
+        self.creature_sprites_group = pygame.sprite.Group()
+        self.food_sprites_group = pygame.sprite.Group()
+        self.creature_sprites_group.add(
             entities.Creature(pygame.math.Vector2(100., 100.),
             pygame.math.Vector2(1, 0))
         )
-        self.sprites_group.add(
+        self.creature_sprites_group.add(
             entities.Creature(pygame.math.Vector2(100., 200.),
             pygame.math.Vector2(1, 0))
         )
 
-        # Create the window
-        self.screen = pygame.display.set_mode(screen_size)
-
         self.init_events()
 
     def init_events(self):
-        pygame.time.set_timer(Events.SPAWN_ITEMS_EVENT, 10000)
+        pygame.time.set_timer(Events.SPAWN_ITEMS_EVENT, 2000)
 
     def spawn_items(self):
-        self.sprites_group.add(
+        self.food_sprites_group.add(
             entities.Food(
                 pygame.math.Vector2(random.randint(0, self.screen_size.x),
                                     random.randint(0, self.screen_size.y)),
@@ -54,18 +55,39 @@ class Wolrd():
     def events(self):
         for event in pygame.event.get():
             if event.type == Events.SPAWN_ITEMS_EVENT:
-                self.logger.debug("Spawn event")
                 self.spawn_items()
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
+    
+    def update_groups(self):
+        self.creature_sprites_group.update()
+        self.food_sprites_group.update()
+
+    def draw_groups(self):
+        self.creature_sprites_group.draw(self.screen)
+        self.food_sprites_group.draw(self.screen)
+
+    def move(self):
+        for creature_sprite in self.creature_sprites_group:
+            nearest_food_vector = None
+            nearest_food_distance = 1000000
+            for food_sprite in self.food_sprites_group:
+                creature_food_vector = food_sprite.position - creature_sprite.position
+                creature_food_distance = creature_food_vector.length()
+                if creature_food_distance < nearest_food_distance:
+                    nearest_food_vector = creature_food_vector
+                    nearest_food_distance = creature_food_distance
+            if nearest_food_vector:
+                creature_sprite.move(nearest_food_vector.normalize())
+    
+    def collision(self):
+        for creature_sprite in self.creature_sprites_group:
+            food_collide_group = pygame.sprite.spritecollide(creature_sprite, self.food_sprites_group, False, pygame.sprite.collide_rect)
+            for food_collide_sprite in food_collide_group:
+                food_collide_sprite.kill()
 
     def game_loop(self):
-
-        # Initialize pygame
-        self.logger.debug("Init PyGame")
-        pygame.init()
 
         # Run the game loop
         while True:
@@ -74,8 +96,10 @@ class Wolrd():
             
             # Handle events
             self.events()
-            self.sprites_group.update()
-            self.sprites_group.draw(self.screen)
+            self.move()
+            self.collision()
+            self.update_groups()
+            self.draw_groups()
 
             # Update the display
             pygame.display.flip()
@@ -88,6 +112,9 @@ def game():
 
     random.seed(1)
     logging.config.fileConfig(fname='logging.conf', disable_existing_loggers=False)
+
+    # Initialize pygame
+    pygame.init()
 
     my_world = Wolrd(pygame.math.Vector2(1600, 1000), 120)
     my_world.game_loop()
